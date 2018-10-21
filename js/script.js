9f13a42642f10e7744881208fa90781fe4dffadc
bitarx <imanishi@bitarx.jp>
2018-10-21 17:57:24 +0900

    テキストエリアに必須チェックを追加

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
-$(function() {
-    $("#sp_menu").click(function() {
-        $("#nav").slideToggle(200);
-    });
-    $('#nav_toggle').click(function() {
-        $('header').toggleClass('open_nav');
-    });
-    $('a[href^=#]').click(function() {
-        var speed = 400; // ミリ秒
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $('body,html').animate({
-            scrollTop: position
-        }, speed, 'swing');
-        return false;
-    });
 
-    // スライドショー
-    $('.single-item').slick({
-	    autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        fade: false,
-        centerMode: true,
-        variableWidth: true,
-    });
-});
+new Vue({
+  el: '#app',
+  data: {
+      edit: {
+        attach_num  : '',
+        new_attach_num : '',
+        removal : '',
+        relocate : '',
+        reserve_date : '',
+      },
+      maxLength: 10
+  },
+  computed: {
+    validation() {
+      const edit = this.edit
+      return {
+        attach_num  : (!!edit.attach_num && edit.attach_num.length <= this.maxLength),
+        new_attach_num  : (!!edit.new_attach_num && edit.new_attach_num.length <= this.maxLength),
+        removal  : (!!edit.removal && edit.removal.length <= this.maxLength),
+        relocate  : (!!edit.relocate && edit.relocate.length <= this.maxLength),
+        reserve_date: (!!edit.reserve_date)
+      }
+    },
+    isValid() {
+      const validation = this.validation
+      return Object
+        .keys(validation)
+        .every(function (key) {
+          return validation[key]
+      })
+    }
+  },
+  methods: {
+    errorClassObject(key) {
+      return {
+        'has-error': (this.validation[key] == false)
+      }
+    },
+    doSubmit() {
+console.log('aaa');
+    }
+  }
+})
